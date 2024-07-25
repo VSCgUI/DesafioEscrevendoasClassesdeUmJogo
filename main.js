d064f35b6851533e7e38e750d04e57952ff95083
Luis Guilherme da Silva Leite <101833932+VSCgUI@users.noreply.github.com>
2024-07-25 11:52:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,60 @@ class hero{
         this.idade = idade,
         this.tipo = tipo
     }
+
+    atacar(){
+        let ataque
     
+        switch(this.tipo){
+            case 'Mago':
+                ataque = 'Magia'
+                break
+            case 'Guerreiro':
+                ataque = 'Espada'
+                break
+            case 'Monge':
+                ataque = 'Artes Marciais'
+                break
+            case 'Ninja':
+                ataque = 'Shuriken'
+                break
+            default:
+                ataque = 'Mãos vazias'
+        }
+    
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+
+
 }
 
-let novoHeroi = new hero('Ziggthy', '25 anos', 'Mago')
+function atacar(){
+    let ataque
+
+    switch(novoHeroi.tipo){
+        case 'Mago':
+            ataque = 'Magia'
+            break
+        case 'Guerreiro':
+            ataque = 'Espada'
+            break
+        case 'Monge':
+            ataque = 'Artes Marciais'
+            break
+        case 'Ninja':
+            ataque = 'Shuriken'
+            break
+        default:
+            ataque = 'Mãos vazias'
+    }
+
+    console.log(`O ${novoHeroi.tipo} atacou usando ${ataque}`)
+}
+
+let novoHeroi = new hero('Ziggthy', '25 anos', 'Ninja')
 console.log(novoHeroi.nome, novoHeroi.idade, novoHeroi.tipo)
 
-if(novoHeroi.tipo === 'Mago'){
-    console.log(`O ${novoHeroi.tipo} atacou usando Magia!`)
-}else if(novoHeroi.tipo === 'Guerreiro'){
-    console.log(`O ${novoHeroi.tipo} atacou usando Espada!`)
-}else if(novoHeroi.tipo === 'Monge'){
-    console.log(`O ${novoHeroi.tipo} atacou usando Artes Marciais!`)
-}else if(novoHeroi.tipo === 'Ninja'){
-    console.log(`O ${novoHeroi.tipo} atacou usando Shuriken!`)
-}
+atacar()
 
 
 
